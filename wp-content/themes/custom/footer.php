3c8c8438882a22ec658ce46e6a7cea7f76945bad
Yamazaki-Ryuji <genjuro23000@gmail.com>
2024-09-08 15:27:50 +0200

    day-2-7

--- wp-content/themes/custom/footer.php
+++ wp-content/themes/custom/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>© 2024 - SHLR <a href="#"><i class="fa-brands fa-instagram-square"></i></a> <a href="#"><i class="fa-brands fa-facebook-square"></i></a></p>
+    <p>© 2024 - SHLR <a href="https://www.instagram.com/shlr.art" target="_blank"><i class="fa-brands fa-instagram-square"></i></a> <a href="https://www.facebook.com/sylvain.lerautreyne" target="_blank"><i class="fa-brands fa-facebook-square"></i></a></p>
 </footer>
 <?php wp_footer();?>
 </body>
